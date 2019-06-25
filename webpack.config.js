5945eb53a397903c6607433b381a259210b2353e
Julio Fuentes <juliofuentes408@gmail.com>
2019-06-24 20:08:30 -0700

    Starting project, environment 50%

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        main: './src/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            { test: /\.js$/, 
+              exclude: /node_modules/, 
+              loader: "babel-loader" 
+            }
+        ]
+    },
+    mode: development
+}
