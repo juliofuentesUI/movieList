7ef2424b5a9f88724882449bbfc6464605d298bf
Julio Fuentes <juliofuentes408@gmail.com>
2019-06-25 19:45:01 -0700

    Changed a message

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<div> FUK U BRANDON </div>, document.getElementById('root'));
+ReactDOM.render(<div> hihihi! </div>, document.getElementById('root'));
